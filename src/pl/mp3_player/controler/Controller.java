5e046cc405fcd6390a20a1a429f7b6e5a71ad2b6
Karol Sidor <karolsidor11@wp.pl>
2018-04-08 20:16:32 +0200

    Events

--- src/pl/mp3_player/controler/Controller.java
+++ src/pl/mp3_player/controler/Controller.java
@@ -1,5 +1,6 @@
 package pl.mp3_player.controler;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -8,49 +9,86 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
-
+import javafx.scene.input.MouseDragEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-        @FXML
-        private Button prevButton;
+    @FXML
+    private Button prevButton;
 
-        @FXML
-        private ToggleButton playButton;
+    @FXML
+    private ToggleButton playButton;
 
-        @FXML
-        private Button nextButton;
+    @FXML
+    private Button nextButton;
 
-        @FXML
-        private Slider volumeSlider;
+    @FXML
+    private Slider volumeSlider;
 
-        @FXML
-        private Slider songSlider;
+    @FXML
+    private Slider songSlider;
 
-        @FXML
-        private TableView<?> contentTable;
+    @FXML
+    private TableView<?> contentTable;
 
-        @FXML
-        private Menu fileMenu;
+    @FXML
+    private Menu fileMenu;
 
-        @FXML
-        private MenuItem fileMenuItem;
+    @FXML
+    private MenuItem fileMenuItem;
 
-        @FXML
-        private MenuItem dirMenuItem;
+    @FXML
+    private MenuItem dirMenuItem;
 
-        @FXML
-        private MenuItem closeMenuItem;
+    @FXML
+    private MenuItem closeMenuItem;
 
-        @FXML
-        private MenuItem aboutMenuItem;
+    @FXML
+    private MenuItem aboutMenuItem;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        configureButtons();
+        configureVolume();
 
     }
+
+    private void configureButtons() {
+        volumeSlider.addEventFilter(MouseDragEvent.MOUSE_PRESSED, new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(javafx.scene.input.MouseEvent event) {
+                System.out.println("Wciśnięto przycisk suwaku głośności");
+            }
+        });
+    }
+
+    private void configureVolume() {
+        prevButton.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                System.out.println("Poprzednia piosenka");
+            }
+        });
+        nextButton.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                System.out.println("Następna piosenka");
+            }
+        });
+        playButton.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                if (playButton.isSelected()){
+                    System.out.println("Play");
+                }else{
+                    System.out.println("Stop");
+                }
+            }
+        });
+    }
+
 }
 
 
