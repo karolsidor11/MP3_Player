c20a3fb0eeb5e4a23253ccfea4a664fecc7abe1c
Karol Sidor <karolsidor11@wp.pl>
2018-04-08 19:22:18 +0200

    Application windows

--- src/pl/mp3_player/main/Main.java
+++ src/pl/mp3_player/main/Main.java
@@ -1,25 +1,25 @@
 package pl.mp3_player.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         final String appName = "Mp3Player v0.1";
-        try {
-            BorderPane root = new BorderPane();
-            Scene scene = new Scene(root,400,400);
-            primaryStage.setTitle(appName);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        Parent parent = FXMLLoader.load(getClass().getResource("/pl/mp3_player/view/Player.fxml"));
+        Scene scene = new Scene(parent);
+        primaryStage.setTitle(appName);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
